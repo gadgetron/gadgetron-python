e11a0d8c3008e6081a5c91b7a480947a3d341638
Kristoffer Langeland Knudsen <kristofferlknudsen@gradientsoftware.net>
2019-09-25 14:58:45 +0200

    Corrected the order of fftshifts in the cfft function suite.

--- gadgetron/util/cfft.py
+++ gadgetron/util/cfft.py
@@ -1,11 +1,12 @@
 
-
 import numpy.fft as fft
 
 
 def cfftn(data, axes):
-    return fft.ifftshift(fft.fftn(fft.fftshift(data, axes=axes)))
+    # Centered fast fourier transform, n-dimensional
+    return fft.fftshift(fft.fftn(fft.ifftshift(data, axes=axes), axes=axes, norm='ortho'), axes=axes)
 
 
 def cifftn(data, axes):
-    return fft.fftshift(fft.ifftn(fft.ifftshift(data, axes), axes=axes))
+    # Centered inverse fast fourier transform, n-dimensional
+    return fft.ifftshift(fft.ifftn(fft.fftshift(data, axes=axes), axes=axes, norm='ortho'), axes=axes)
